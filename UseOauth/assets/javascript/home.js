4dc0c3cb0756e588888a0f4a98573f31f738d92e
HARVEYNASH\sangdangq <Sang.DangQuang@nashtechglobal.com>
2018-07-10 18:24:22 +0700

    fix bug

--- UseOauth/assets/javascript/home.js
+++ UseOauth/assets/javascript/home.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementsByClassName
+    let collapse = document.getElementsByClassName("collapse-menu");
+    let dropdownVertical = document.getElementsByClassName("dropdown-left");
+    collapse[0].addEventListener("click", function() {
+        // collapse[0].style.display = "none";
+
+        if(dropdownVertical[0].style.display == "block") {dropdownVertical[0].style.display = "none"}
+        else {dropdownVertical[0].style.display = "block"}
+        
+        console.log(dropdownVertical[0].style.display);
+        
+    })
 },false)
