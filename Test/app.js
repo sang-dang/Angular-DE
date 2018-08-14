3da0075e7348cb99ce3c6194da0eed26e3451726
sang-dang <37040288+sang-dang@users.noreply.github.com>
2018-08-14 22:13:23 +0700

    Update app.js

--- Test/app.js
+++ Test/app.js
@@ -1,11 +1,14 @@
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
 
 http.createServer((req, res) => {
     let pathname = url.parse(req.url, true).pathname;
-    if (pathname === "/") {
-        res.end("FINISH");
-    }
+    fs.readFile('./index.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
 }).listen(5000, function() {
     console.log("Server is listening port 5000");
 });
