ed35802a5104ba02157924a80a240078bb312392
Sang Dang <sang.a5vy2@gmail.com>
2018-11-18 13:09:59 +0700

    add main UI

---
+++ ServerTest/app.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+http.createServer((req, res) => {
+    let pathname = url.parse(req.url, true).pathname;
+    let content;
+    fs.readFile("./main.html", (err, data) => {
+        this.content = data;
+    })
+    if (pathname === "/") {
+        res.end(content);
+    }
+}).listen(5000, function() {
+    console.log("Server is listening port 5000");
+});
